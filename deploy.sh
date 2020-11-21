f2f251b963a5fa2f256f534a383cb2a37c628fcf
chenxiaomo <xiaomo_0258@icloud.com>
2020-11-21 18:50:27 +0800

    [更新] 全面更新docker-compose

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ GITCOMMITHASH=`git rev-parse --short HEAD`
 export GITHASH=${GITCOMMITHASH}
 
 # 容器服务
-service=("student_server" "admin_server" "teacher_server")
+services=("student_server" "admin_server" "teacher_server")
 
 version(){
   echo "=======项目名称:${COMPOSE_PROJECT_NAME}  版本号：${GITHASH}========"
